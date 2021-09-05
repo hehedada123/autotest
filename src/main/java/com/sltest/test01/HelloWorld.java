671d0489e1e5b3cfcee9702ecfeab28d64c5d19b
sunli <787505794@qq.com>
2021-09-05 16:20:53 +0800

    第一次提交测试项目

---
+++ src/main/java/com/sltest/test01/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sltest.test01;
+
+/**
+ * @Package:com.sltest.test01
+ * @Name:HelloWorld
+ * @date:2021/9/4 9:11
+ * @Description:TODO
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+    }
+}
